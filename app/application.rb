2c621e573d7ecb338c896305679da3cf69aa35fc
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-16 02:19:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,13 +7,13 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      
+
       item_name = req.path.split("items").last
       item = @@items.find{|i| i.name == item_name}
 
       if item.nil?
         resp.write "Item not found"
-        resp.status = 400
+        resp.status = "400"
 
       else
         resp.write item.price
