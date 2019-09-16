6b4f5eb8cd8744b8c3fca857ca3e8ef7feae938c
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-16 01:42:26 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    resp = Rack:Request.new(env)
+
+    if req.path=="/items"
+      resp.write "You requested the items"
+    else
+      resp.write "Route not found"
+      resp.status = "404"
+    end
+    resp.finish
+  end
+end
