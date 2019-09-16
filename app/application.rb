5961e51a0e3028b2523603b4c03a8298c0212f6b
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-16 02:14:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,12 +14,13 @@ class Application
         resp.write "Item not found"
         resp.status = 400
 
-      else resp.write item.price
+      else
+        resp.write item.price
       end
 
     else
       resp.write "Route not found"
-      resp.status = "404"
+      resp.status = 404
     end
     resp.finish
   end
