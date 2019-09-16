86f8c77ae85320efa1464b765f51a7da9cb30464
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-16 02:01:44 +0000

    Automatically backed up by Learn

--- app/item.rb
+++ app/item.rb
@@ -5,17 +5,4 @@ class Item
     @name = name
     @price = price
   end
-
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
-
-    if req.path=="/items"
-      resp.write "You requested the items"
-    else
-      resp.write "Item not found"
-      resp.status = "400"
-    end
-    resp.finish
-  end
 end
