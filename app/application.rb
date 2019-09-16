ee0cb47f9623987dd2f2fccb50ae1c6e21d6920e
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-16 02:22:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,7 +13,6 @@ class Application
       if item.nil?
         resp.write "Item not found"
         resp.status = 400
-
       else
         resp.write item.price
       end
@@ -22,6 +21,7 @@ class Application
       resp.write "Route not found"
       resp.status = 404
     end
+    
     resp.finish
   end
 end
