8a927289976383745100f2a5b39af494f2a69916
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-16 01:49:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    req = Rack:Request.next
+    req = Rack:Request.new(env)
 
     if req.path=="/items"
       resp.write "You requested the items"
